e7ec629dce3ac13dc28b0df3cc5c49400abd040a
unknown <tristan.scheidemann@live.com>
2021-03-13 19:30:57 +0100

    Fixed bugs in PDE model. It is probably still not giving correct results but at least it is running

--- models/main_seird_pde.cpp
+++ models/main_seird_pde.cpp
@@ -3,6 +3,27 @@
 #include<vector>
 #include <iostream>
 
+template<class T>
+void set_radial_values(T& u0, typename T::value_type x_points, typename T::value_type y_points, typename T::value_type dimX, typename T::value_type dimY,typename T::value_type hx, typename T::value_type radius, typename T::value_type val,int current_dimension) {
+	using F = typename T::value_type;
+	for (int i = 0; i < x_points; i++) {
+		for (int j = 0; j < y_points; j++) {
+			F worldX = ((i + 1.0) / x_points) * dimX;
+			F worldY = ((j + 1.0) / y_points) * dimY;
+			int offset = current_dimension*x_points*y_points;
+			F a = (worldX - 0.5 * dimX);
+			F b = (worldY - 0.5 * dimY);
+			F r = 0.1;
+			if ((a * a + b * b) <= r) {
+				u0[i * x_points + j+offset] = val;
+			}
+			else {
+				u0[i * x_points + j+offset] = 0;
+			}
+		}
+
+	}
+}
 
 template<class T>
 T initial_values(typename T::value_type dimX, typename T::value_type dimY, typename T::value_type hx) {
@@ -13,27 +34,34 @@ T initial_values(typename T::value_type dimX, typename T::value_type dimY, typen
 
 	size_t nVars = ((dimX / hx) + 1) * ((dimY / hx) + 1);
 	std::vector<F> u0(nVars*5,F(0)); //number of vertices in discretization
-/*
-	F radius = 0.5;
-	for (int i = 0; i < x_points; i++) {
-		for (int j = 0; j < y_points; j++) {
-			F worldX = ((i + 1) / x_points)*dimX;
-			F worldY = ((j + 1) / y_points) * dimY;
+	
+	//Sets initial values for first dimension
+	F r1 = 0.1;
+	F v1 = 100;
+	set_radial_values(u0, x_points, y_points, dimX, dimY, hx, r1, v1,0);
 
-			F a = (worldX - 0.5 * dimX);
-			F b = (worldY - 0.5 * dimY);
-			F r = 0.5;
-			if ((a*b+b*b) <= r) {
-				//u0[i*x_points]
-			}
-		}
-		
-	}
-*/
+	//Sets initial values for second dimension
+	F r2 = 100;
+	F v2 = 20;
+	set_radial_values(u0, x_points, y_points, dimX, dimY, hx, r2,v2, 1);
+
+
 	return u0;
-
 }
 
+template<class T>
+void print_initialvalues(const T& u,  typename T::value_type dimX, typename T::value_type dimY, typename T::value_type hx, int current_dimension) {	
+	size_t x_points = (dimX / hx) + 1;
+	size_t y_points = (dimY / hx) + 1;
+	int offset = current_dimension * x_points * y_points;
+	for (int i = 0; i < x_points; i++) {
+		
+		for (int j = 0; j < y_points; j++) {
+			std::cout << u[i * x_points + j+offset]<< "\t";			
+		}
+		std::cout << "\n";	
+	}	
+}
 
 int main() {
 
@@ -43,14 +71,22 @@ int main() {
 	double sigma = 0.1;
 	double rho = 0.1;
 
-
-
 	ug::epi::SEIRD<std::vector<double>,ug::epi::seird::Geometry::Plane> seird_model(alpha, kappa, theta, sigma,rho);
 
 //	std::vector<double> u0 = { 753056,2714,0,0,72 };
 //	std::vector<double> u0 = { 753056,2714,0,0,72 };
 	double t_start = 0;
 	double t_end = 42;
+	std::vector<double> u0 =  initial_values<std::vector<double>>(1, 1, 0.1);
+	std::cout << "Initial values on the grid:" << "\n";
+	for (int i = 0; i < 5; i++) {
+		std::cout << "Initial values dimension: " << i << "\n";
+		print_initialvalues(u0, 1, 1, 0.1, i);
+		std::cout << "\n";
+	}
 
-	//auto [timepoints, data] = seird_model.run(t_start, u0, t_end);
+	seird_model.change_step_size_spatial(0.1);
+	seird_model.change_step_size_time(0.1);
+
+	auto [timepoints, data] = seird_model.run(t_start, initial_values<std::vector<double>>, t_end);
 }
--- models/seird_pde.h
+++ models/seird_pde.h
@@ -1,5 +1,7 @@
 #pragma once
 #include <vector>
+#include <string>
+#include <array>
 
 namespace ug {
 	namespace epi {
@@ -19,12 +21,12 @@ namespace ug {
 		class SEIRD<T,seird::Geometry::Plane> {
 			using F = typename T::value_type;
 		private:
-
 			F rho = 1;
 			F alpha = 1;
 			F sigma = 1;
 			F kappa = 1;
 			F theta = 1;
+			F tau = 1;
 			F dimX = 1;
 			F dimY = 1;
 			F D=1;
@@ -33,107 +35,150 @@ namespace ug {
 			F ht = 0.25;
 
 
-		T seird_ode_system(F t, std::array<F, 5>& u) {
+		T seird_pde_to_ode(T& u) {
 			size_t vars_per_dim = ((dimX / hx) + 1) * ((dimY / hx) + 1);
-			T res(*dimModel*vars_per_dim); //number of vertices in discretization
+			size_t vars_per_row = ((dimX / hx) + 1);
+			size_t vars_per_column = ((dimY / hx) + 1);
+			T res(dimModel*vars_per_dim,F(0.0)); //number of vertices in discretization
 			auto curr = res.begin();
 			auto G = u.begin(); // Gesunde (Susceptibles)
-			auto A = G+dim; // Angesteckte (Exposed)
-			auto K = A+dim; // Kranke (Infected)
-			auto R = K+dim; // Erholte (Recovered)
-			auto V = V+dim; // Verstorbene (Deaths)
-			set_susceptibles(curr,G,A, vars_per_dim);
+			auto A = G+ vars_per_dim; // Angesteckte (Exposed)
+			auto K = A+ vars_per_dim; // Kranke (Infected)
+			auto R = K+ vars_per_dim; // Erholte (Recovered)
+			auto V = R+ vars_per_dim; // Verstorbene (Deaths)
+
+			set_susceptibles(curr,G,A, vars_per_row,vars_per_column);
 			curr += vars_per_dim;
-			set_exposed(curr, A, G, vars_per_dim);
+			set_exposed(curr, A, G, vars_per_row, vars_per_column);
 			curr += vars_per_dim;
-			set_infected(curr, K, A, vars_per_dim);
+			set_infected(curr, K, A, vars_per_row, vars_per_column);
 			curr += vars_per_dim;
-			set_recovered(curr, A, K, vars_per_dim);
+			set_recovered(curr, A, K, vars_per_row, vars_per_column);
 			curr += vars_per_dim;
-			set_deaths(curr, K, vars_per_dim);
+			set_deaths(curr, K, vars_per_row, vars_per_column);
 			return res;
 		}
 
-		void set_susceptibles(typename T::iterator G_prime, typename T::iterator G, typename T::iterator A, int rowDim) {
+		void set_susceptibles(typename T::iterator G_prime, typename T::iterator G, typename T::iterator A, int nCols, int nRows) {
 			F hinv = 1 / (hx * hx);
-			for (int i = 0; rowDim; i++) {
-				for (int j = 0; j < rowDim;j++) {
-					F diffusion =D* hinv * (G[(i - 1) * rowDim + j] - 4 * G[i * rowDim + j] + G[(i + 1) * rowDim + j] + G[i * rowDim + j - 1] + G[i * rowDim + j + 1]);
-					G_prime[i * rowDim + j] = diffusion - alpha * A[i * rowDim + j] * G[i * rowDim + j];
+			for (int i = 0; i<nRows; i++) {
+				for (int j = 0; j < nCols;j++) {
+					F diffusion =D* hinv * (G[(i - 1) * nCols + j] - 4 * G[i * nCols + j] + G[(i + 1) * nCols + j] + G[i * nCols + j - 1] + G[i * nCols + j + 1]);
+					G_prime[i * nCols + j] = diffusion - alpha * A[i * nCols + j] * G[i * nCols + j];
 				}
 			}
 		}
-		void set_exposed(typename T::iterator A_prime, typename T::iterator A, typename T::iterator G, int rowDim) {
+		void set_exposed(typename T::iterator A_prime, typename T::iterator A, typename T::iterator G, int nCols, int nRows) {
 			F hinv = 1 / (hx * hx);
 			F tauinv = 1 / tau;
-			for (int i = 0; rowDim; i++) {
-				for (int j = 0; j < rowDim; j++) {
-					F diffusion = D * hinv * (A[(i - 1) * rowDim + j] - 4 * A[i * rowDim + j] + A[(i + 1) * rowDim + j] + A[i * rowDim + j - 1] + A[i * rowDim + j + 1]);
-					A_prime[i * rowDim + j] = diffusion + alpha * A[i * rowDim + j] * G[i * rowDim + j]-tauinv*A[i*rowDim+j];
+			for (int i = 0; i < nRows; i++) {
+				for (int j = 0; j < nCols; j++) {
+					F diffusion = D * hinv * (A[(i - 1) * nCols + j] - 4 * A[i * nCols + j] + A[(i + 1) * nCols + j] + A[i * nCols + j - 1] + A[i * nCols + j + 1]);
+					A_prime[i * nCols + j] = diffusion + alpha * A[i * nCols + j] * G[i * nCols + j]-tauinv*A[i*nCols+j];
 				}
 			}
 		}
-		void set_infected(typename T::iterator K_prime, typename T::iterator K, typename T::iterator A, int rowDim) {
+		void set_infected(typename T::iterator K_prime, typename T::iterator K, typename T::iterator A, int nCols, int nRows) {
 			F sigmainv = 1 / sigma;
 			F tauinv = 1 / tau;
-			for (int i = 0; rowDim; i++) {
-				for (int j = 0; j < rowDim; j++) {
-					K_prime[i * rowDim + j] =kappa* tauinv * A[i * rowDim + j]-sigmainv*K[i*rowDim+j];
+			for (int i = 0; i < nRows; i++) {
+				for (int j = 0; j < nCols; j++) {
+					K_prime[i * nCols + j] =kappa* tauinv * A[i * nCols + j]-sigmainv*K[i*nCols+j];
 				}
 			}
 		}
 
-		void set_recovered(typename T::iterator R_prime, typename T::iterator A, typename T::iterator K, int rowDim) {
+		void set_recovered(typename T::iterator R_prime, typename T::iterator A, typename T::iterator K, int nCols, int nRows) {
 			F hinv = 1 / (hx * hx);
 			F sigmainv = 1 / sigma;
 			F tauinv = 1 / tau;
-			for (int i = 0; rowDim; i++) {
-				for (int j = 0; j < rowDim; j++) {
-					F diffusion = -D * hinv * (A[(i - 1) * rowDim + j] - 4 * A[i * rowDim + j] + A[(i + 1) * rowDim + j] + A[i * rowDim + j - 1] + A[i * rowDim + j + 1]);
-					R_prime[i * rowDim + j] = diffusion + alpha * (1-kappa)*tauinv*A[i * rowDim + j] + sigmainv*(1-theta) * K[i * rowDim + j];
+			for (int i = 0; i < nRows; i++) {
+				for (int j = 0; j < nCols; j++) {
+					F diffusion = -D * hinv * (A[(i - 1) * nCols + j] - 4 * A[i * nCols + j] + A[(i + 1) * nCols + j] + A[i * nCols + j - 1] + A[i * nCols + j + 1]);
+					R_prime[i * nCols + j] = diffusion + alpha * (1-kappa)*tauinv*A[i * nCols + j] + sigmainv*(1-theta) * K[i * nCols + j];
 				}
 			}
 		}
 
-		void set_deaths(typename T::iterator D_prime,typename T::iterator K, int rowDim) {
+		void set_deaths(typename T::iterator D_prime,typename T::iterator K, int nCols, int nRows) {
 			F sigmainv = 1 / sigma;
 			F tauinv = 1 / tau;
-			for (int i = 0; rowDim; i++) {
-				for (int j = 0; j < rowDim; j++) {
-					D_prime[i * rowDim + j] = sigmainv*theta*K[i * rowDim + j];
+			for (int i = 0; i < nRows; i++) {
+				for (int j = 0; j < nCols; j++) {
+					D_prime[i * nCols + j] = sigmainv*theta*K[i * nCols + j];
 				}
 			}
 		}
+
+		void get_new_point(T& dest, const T& source1,const T& source2,F h, F factor) {
+			int n = source1.size();
+			for (int i = 0; i < n;i++) {
+				dest[i]=source1[i] + h*factor*source2[i];
+			}
+		}
+
+		void add_k_to_vector(T& dest, const T& source, F h, F factor) {
+			int n = source.size();
+			for (int i = 0; i < n; i++) {
+				dest[i] += h * factor * source[i];
+			}
+		}
+
+
+		//As ODE system does not depend on time, the ODE is autonomous
+		void calc_values(T& u, T& res,T& v) {
+		
+			T ks = seird_pde_to_ode(u);
+			
+			get_new_point(v, u, ks, hx, 0.5);
+			add_k_to_vector(u, ks, hx, (1.0 / 6.0));
+			ks = seird_pde_to_ode(v);
+			get_new_point(v, u, ks, hx, 0.5);
+			add_k_to_vector(u, ks, hx, (2.0 / 6.0));
+			ks = seird_pde_to_ode(v);
+			get_new_point(v, u, ks, hx, 1);
+			add_k_to_vector(u, ks, hx, (2.0 / 6.0));
+			ks = seird_pde_to_ode(v);
+	
+			for (int i = 0; i < u.size(); i++) {
+				res.push_back(u[i]);
+			}
+			
+		}
+
 		public:
-			SEIRD( F _alpha, F _kappa, F _theta, F _sigma, F _rho ):rho(_rho), alpha(_alpha), sigma(_sigma), kappa(_kappa) {
+			SEIRD( F _alpha, F _kappa, F _theta, F _sigma, F _tau ):tau(_tau), alpha(_alpha), sigma(_sigma), kappa(_kappa) {
 
 			}
+			const std::array<std::string, 5> names = { "Susceptibles","Exposed", "Infected", "Recovered", "Deaths" };
+
 			void change_step_size_time(F _ht) {
 				ht = _ht;
 			}
 
-			void change_step_size_spatial(F hx) {
+			void change_step_size_spatial(F _hx) {
 				hx= _hx;
 			}
-			auto run(F t0, const T (&u0)(size_t dimX, size_t dimY, F hx), F tend) {
+			auto run(F t0,  T (&u0)(F dimX,F dimY, F hx), F tend) {
 				size_t vars_per_dim = ((dimX/hx)+1)*((dimY/hx)+1);
-				std::vector<F> u(dimModel*vars_per_dim); //number of vertices in discretization
+				
 				std::vector<F> u = u0(dimX, dimY, hx);
 				std::vector<F> res;
 				std::vector<F> ts;
 				ts.push_back(t0);
-				F t = t0 + h;
-				/*
+				F t = t0 + ht;
+				std::vector<F> temp(u.size());
+				
 				while (t <= tend) {
-					calc_values(t, u, res);
+					calc_values(u, res,temp);
 					ts.push_back(t);
-					t += h;
+					t += ht;
 				}
 				if (t != tend) {
-					calc_values(t, u, res);
+					calc_values(u, res, temp);
 					ts.push_back(tend);
 				}
-				*/
+				
 				return std::make_tuple(ts, res);
 			}
 		
