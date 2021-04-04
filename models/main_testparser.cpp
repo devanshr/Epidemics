e80fb22a3de632cbc4a5b098b17830525d29a7e4
unknown <tristan.scheidemann@live.com>
2021-04-04 12:44:17 +0200

    Added parse test problem

---
+++ models/main_testparser.cpp
@@ -0,0 +1,33 @@
+#include "../../ConstrainedOptimization/core/parameter_estimation.h"
+#include<vector>
+#include <iostream>
+
+
+int main(){
+
+	std::string dir="C:/Users/Annett/Desktop/Epidemics Git/testoptimization_pde/";
+	co::EpidemicsPDEEvaluation<co::EFloat64,co::ConfigComputation::Local, co::ConfigOutput::File> evaluator(dir,"subset_target.lua", "subset_sim.lua");
+	
+	
+	std::vector<double> times;
+	std::vector<double> positions;
+	std::vector<double> data;
+	
+	co::utility::parse_csv_table_times_pde(dir, "subset_target.lua", data, positions,times);
+	
+	std::cout<<"times\n";
+	for (auto &x:times){
+		std::cout<<x<<"\t";
+	}
+	
+	std::cout<<"\n\npositions\n";
+	for (auto &x:positions){
+		std::cout<<x<<"\t";
+	}
+
+
+	std::cout<<"\n\ndata\n";
+	for (auto &x:data){
+		std::cout<<x<<"\t";
+	}	
+}
