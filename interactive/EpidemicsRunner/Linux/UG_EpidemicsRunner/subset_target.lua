b9bbd540dbc414619b67d57538e7a8e60b72b5da
RefreshingAutoMate <refreshingautomate@gmail.com>
2021-05-03 21:33:31 +0200

    Cleanup

--- interactive/EpidemicsRunner/Linux/UG_EpidemicsRunner/subset_target.lua
+++
@@ -1,8 +0,0 @@
-# Specifiy the selected coloumns from the Experimental data that are used for the optimization.
-# The first 3 columns refer to time, posX,posY
-# Please add addtional files in the order shown. 
-
-filename="expdata/Positive.txt"
-selected_columns=[0,1]
-filename="expdata/Verstorbene.txt"
-selected_columns=[1]
