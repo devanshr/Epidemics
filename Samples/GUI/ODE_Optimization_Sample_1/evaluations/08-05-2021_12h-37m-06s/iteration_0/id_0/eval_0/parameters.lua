24e33cbcbe50d8c301be6b775bf1527f5e8c5731
devanshr <devanshr@protonmail.com>
2021-05-08 17:09:20 +0200

    clean up

--- Samples/GUI/ODE_Optimization_Sample_1/evaluations/08-05-2021_12h-37m-06s/iteration_0/id_0/eval_0/parameters.lua
+++
@@ -1,2 +0,0 @@
-parameters={alpha=EVar64(EFloat64(0.127716975835), EFloat64(1e-06),EFloat64(0.5)),
-}
--- Samples/GUI/ODE_Optimization_Sample_1/evaluations/08-05-2021_12h-37m-06s/iteration_0/id_1/eval_0/parameters.lua
+++
@@ -1,2 +0,0 @@
-parameters={alpha=EVar64(EFloat64(0.448754908075), EFloat64(1e-06),EFloat64(0.5)),
-}
