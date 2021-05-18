fa7d4524239670bdc2371c03c277c123d4eb6b18
HeinzHermann <marvin.glaser91@gmail.com>
2021-05-18 18:21:09 +0200

    added timestamps to heatmap save files

---
+++ interactive/EpidemicsRunner/Linux/UG_EpidemicsRunner/comp.sh
@@ -0,0 +1,3 @@
+#clear; rm Epidemics.bin; g++ -O3 -o Epidemics.bin main.cpp `pkg-config --cflags --libs gtk+-3.0` -export-dynamic;./Epidemics.bin
+rm ~/Documents/test/*;
+clear; rm Epidemics.bin; g++ -O3 -mavx -mfma -o Epidemics.bin main.cpp `pkg-config --cflags --libs gtk+-3.0` -export-dynamic;./Epidemics.bin
